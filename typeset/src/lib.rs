08f8ef1cd9658e6ab728964204f38f805ef03187
bennyboer <barny.eder@gmail.com>
2022-07-18 13:18:28 +0200

    Documented what linearization is

--- typeset/src/lib.rs
+++ typeset/src/lib.rs
@@ -1,1 +1,14 @@
-pub mod linearization;
+use crate::linearization::Block;
+use document::structure::DocumentStructure;
+
+pub(crate) mod linearization;
+
+/// Linearization of a document structure is the process
+/// of turning a document tree into a flat list of "blocks".
+/// Blocks in turn are an abstract representation of a group
+/// of elements that belong together in a document.
+/// An example for a block would be a paragraph that contains of words/sentences/glyphs
+/// or an image together with a caption, etc.
+pub(crate) fn linearize(document_structure: &DocumentStructure) -> Vec<Block> {
+    vec![]
+}
